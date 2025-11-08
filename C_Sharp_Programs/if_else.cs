40440d07d8ae28fa772f95336876f68583b74a76
Vaishnavi Adat <vaishnaviadat@gmail.com>
2025-11-08 20:27:45 +0530

    C# program

---
+++ C_Sharp_Programs/if_else.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Programs
+{
+    internal class if_else
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Enter Your Age:");
+            //int age=Convert.ToInt32(Console.ReadLine());
+            //if(age>=18)
+            //{
+            //    Console.WriteLine("Eligible for vote");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Not Eligible for vote");
+            //}
+
+            //Console.WriteLine("Enter String:");
+            //string name=Console.ReadLine();
+            //string name1 = "vaishna";
+            //if(name==name1)
+            //{
+            //    Console.WriteLine("Welcome");
+            //}
+            //else
+            //{
+            //    {
+            //        Console.WriteLine("who are you");
+            //    }
+            //}
+            //Console.WriteLine("Enter Username:");
+            //string user_name = Console.ReadLine();
+            //Console.WriteLine("Enter Password:");
+            //string password = Console.ReadLine();
+            //if(user_name == "vaishna" && password=="abcd")
+            //{
+            //    Console.WriteLine("Login Succesfully");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("wrong credital");
+            //}
+            Console.WriteLine("Enter number:");
+            int num1=int.Parse(Console.ReadLine());
+            if(num1>0)
+            {
+                Console.WriteLine("Positive number");
+            }
+            else if(num1<0)
+            {
+                Console.WriteLine("Negative  number");
+            }
+            else
+            {
+                Console.WriteLine("netural");
+            }
+                Console.ReadLine();
+        }
+
+    }
+}
---
+++ C_Sharp_Programs/if_elseif.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Programs
+{
+    internal class if_elseif
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter customer type:");
+            string Cust_type=Console.ReadLine();
+            if(Cust_type == "premium")
+            {
+                Console.WriteLine("Enter payment amount:");
+                int pay_amount=int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter pay_mode:");
+                string pay_mode=Console.ReadLine();
+                if(pay_amount>10000 && pay_mode=="cash" )
+                {
+                    Console.WriteLine("payment amount more than 10000 and discount:20%");
+                }
+                else if(pay_amount > 10000 && pay_mode == "card")
+                {
+                    Console.WriteLine("payment amount more than 10000 and discount:15%");
+                }
+                else if ((pay_amount < 10000 || pay_amount == 10000 ) && (pay_mode == "any"))
+                {
+                    Console.WriteLine(" payment amount 10000 or less and discount:10%");
+                }
+            }
+            else 
+            {
+                Console.WriteLine("Enter payment amount:");
+                int pay_amount = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter pay_mode:");
+                string pay_mode =(Console.ReadLine());
+                if (pay_amount > 5000 && pay_mode == "cash")
+                {
+                    Console.WriteLine("payment amount more than 5000 and discount:5%");
+                }
+                else if (pay_amount > 5000 && pay_mode == "card")
+                {
+                    Console.WriteLine("payment amount more than 5000 and discount:3%");
+                }
+                else if ((pay_amount < 5000 || pay_amount == 5000) &&(pay_mode == "any"))
+                {
+                    Console.WriteLine(" payment amount 5000 or less and discount:no");
+                }
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
