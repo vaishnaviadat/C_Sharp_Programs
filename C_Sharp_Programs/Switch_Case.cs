186963398bb94986f6aefd4ecb4519058782f191
Vaishnavi Adat <vaishnaviadat@gmail.com>
2025-11-09 13:07:43 +0530

    C# Program

---
+++ C_Sharp_Programs/Switch_Case.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Programs
+{
+    internal class Switch_Case
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Weekday:");
+            int week_no =int.Parse(Console.ReadLine());
+            switch(week_no)
+            {
+                case 1:Console.WriteLine("Monday");
+                    break;
+                case 2:Console.WriteLine("Tuesday");
+                    break;
+                case 3:Console.WriteLine("Wednesday");
+                    break;
+                case 4:Console.WriteLine("Friday");
+                    break;
+                case 5:Console.WriteLine("Saturday");
+                    break;
+                case 6:Console.WriteLine("Sunday");
+                    break;
+               default:Console.WriteLine("Wrong Choice");
+                    break;
+                
+            }
+            Console.ReadLine();
+        }
+
+    }
+}
