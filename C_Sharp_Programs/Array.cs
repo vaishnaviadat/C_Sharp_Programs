e87101147c1b0fe3d48bfa0560c92749aaaccda2
Vaishnavi Adat <vaishnaviadat@gmail.com>
2025-11-11 17:25:51 +0530

    C# Program

--- C_Sharp_Programs/Array.cs
+++ C_Sharp_Programs/Array.cs
@@ -11,10 +11,25 @@ namespace C_Sharp_Programs
         static void Main(string[] args)
         {
             // show index with elements
-            int[] number = { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100 };
-            for(int i=0;i<number.Length;i++)
+            //int[] number = { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100 };
+            //for(int i=0;i<number.Length;i++)
+            //{
+            //    Console.WriteLine($"Elements index is:{i}= {number[i]}");
+            //}
+
+            //input from user
+            int[] mark = new int[5];
+            Console.WriteLine("Enter Marks:"); 
+            for(int i=0;i<mark.Length;i++)
             {
-                Console.WriteLine($"Elements index is:{i}= {number[i]}");
+                Console.WriteLine($"Enter mark for subject {i+1}:");
+                mark[i]=int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Enterd Marks:");
+
+            for (int i=0;i<mark.Length;i++)
+            {
+                Console.WriteLine($"Marks for subject {i+1}: {mark[i]}");
             }
             Console.ReadLine();
         }
