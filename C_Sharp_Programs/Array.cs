28a5d17bc36e8c117a4e69f5af048a84991cd8a4
Vaishnavi Adat <vaishnaviadat@gmail.com>
2025-11-11 16:53:59 +0530

    C# Program

--- C_Sharp_Programs/Array.cs
+++ C_Sharp_Programs/Array.cs
@@ -10,7 +10,12 @@ namespace C_Sharp_Programs
     {
         static void Main(string[] args)
         {
-            
+            // show index with elements
+            int[] number = { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100 };
+            for(int i=0;i<number.Length;i++)
+            {
+                Console.WriteLine($"Elements index is:{i}= {number[i]}");
+            }
             Console.ReadLine();
         }
         
