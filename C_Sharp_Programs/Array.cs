96bf049a12c67b62c9388d9c8197065746b77c90
Vaishnavi Adat <vaishnaviadat@gmail.com>
2025-11-12 15:24:24 +0530

    C# Program

--- C_Sharp_Programs/Array.cs
+++ C_Sharp_Programs/Array.cs
@@ -10,6 +10,20 @@ namespace C_Sharp_Programs
     {
         static void Main(string[] args)
         {
+            //input from user
+            //int[] arr = new int[5];
+            //Console.WriteLine("Enter 5 number:");
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    Console.WriteLine($"Enter number {i + 1}:");
+            //    arr[i] = int.Parse(Console.ReadLine());
+            //}
+            //Console.WriteLine("You entered:");
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    Console.WriteLine(arr[i]);
+            //}
+
             // show index with elements
             //int[] number = { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100 };
             //for(int i=0;i<number.Length;i++)
@@ -64,30 +78,72 @@ namespace C_Sharp_Programs
             //}
 
 
-            Console.WriteLine("Enter 2 Students Marks:");
-            int[,] mark = new int[2, 3]; // 2 students, 3 subjects each
+            //Console.WriteLine("Enter 2 Students Marks:");
+            //int[,] mark = new int[2, 3]; // 2 students, 3 subjects each
 
-            // Input marks
-            for (int i = 0; i < 2; i++)
+            //// Input marks
+            //for (int i = 0; i < 2; i++)
+            //{
+            //    Console.WriteLine($"\nStudent {i + 1}:");
+            //    for (int j = 0; j < 3; j++)
+            //    {
+            //        Console.Write($"Enter Marks {j + 1}: ");
+            //        mark[i, j] = int.Parse(Console.ReadLine());
+            //    }
+            //}
+
+            //// Display marks
+            //Console.WriteLine("\nMarks entered:");
+            //for (int i = 0; i < 2; i++)
+            //{
+            //    Console.WriteLine($"\nStudent {i + 1}:");
+            //    for (int j = 0; j < 3; j++)
+            //    {
+            //        Console.WriteLine($"Subject {j + 1}: {mark[i, j]}");
+            //    }
+            //}
+
+            //jagged Array
+            //int[][] jaggedArr = new int[3][];
+            //jaggedArr[0] = new int[2] { 1, 2 };
+            //jaggedArr[1] = new int[3] { 3, 4, 5 };
+            //jaggedArr[2] = new int[4] { 6, 7, 8, 9 };
+            //Console.WriteLine("Jagged Array:"); 
+            //for(int i=0;i<jaggedArr.GetLength(0);i++)
+            //{
+            //    Console.Write($"Element {i}: ");
+            //    for(int j=0;j<jaggedArr[i].Length;j++)
+            //    {
+            //        Console.Write(jaggedArr[i][j] + " ");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            //input from user
+            Console.WriteLine("Enter students:");
+            int size = int.Parse(Console.ReadLine());
+            int[][] jarr= new int[size][];
+            for (int i=0;i<size;i++)
             {
-                Console.WriteLine($"\nStudent {i + 1}:");
-                for (int j = 0; j < 3; j++)
+                Console.WriteLine($"Enter number of subjects for student {i+1}:");
+                int subjects = int.Parse(Console.ReadLine());
+                jarr[i] = new int[subjects];
+                for(int j=0;j<subjects;j++)
                 {
-                    Console.Write($"Enter Marks {j + 1}: ");
-                    mark[i, j] = int.Parse(Console.ReadLine());
+                    Console.WriteLine($"Enter marks for subject {j+1}:");
+                    jarr[i][j]= int.Parse(Console.ReadLine());
+                }
+            }
+            Console.WriteLine("Entered Marks:");
+            for(int i=0;i<size;i++)
+            {
+                Console.WriteLine($"Student {i+1}:");
+                for(int j=0;j<jarr[i].Length;j++)
+                {
+                    Console.WriteLine($"Subject {j+1}: {jarr[i][j]}");
                 }
             }
 
-            // Display marks
-            Console.WriteLine("\nMarks entered:");
-            for (int i = 0; i < 2; i++)
-            {
-                Console.WriteLine($"\nStudent {i + 1}:");
-                for (int j = 0; j < 3; j++)
-                {
-                    Console.WriteLine($"Subject {j + 1}: {mark[i, j]}");
-                }
-            }
 
             Console.ReadLine();
         }
