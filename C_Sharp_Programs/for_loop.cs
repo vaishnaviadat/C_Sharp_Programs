90c9c381ce21ab5c0b35e57ca494bb3e676cd4b3
Vaishnavi Adat <vaishnaviadat@gmail.com>
2025-11-17 20:45:22 +0530

    C# Program

---
+++ C_Sharp_Programs/for_loop.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Programs
+{
+    internal class for_loop
+    {
+        static void Main(string[] args)
+        {
+            // For Loop
+            //int sum1 = 0;
+            //Console.WriteLine("............for loop..........");
+
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    sum1 += i;
+            //}
+
+            //Console.WriteLine("Sum of 1 to 10 is = " + sum1);
+
+            //Console.WriteLine("Enter table:");
+            //int num1 = int.Parse(Console.ReadLine());
+
+
+            //nested for loop
+            //Console.WriteLine("............nested for loop..........");
+            //for (int i=1;i<=10;i++)
+            //{
+            //    for(int j=1;j<=10;j++)
+            //    {
+            //        Console.Write(i * j+"\t");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            Console.ReadLine();
+        }
+    }
+}
