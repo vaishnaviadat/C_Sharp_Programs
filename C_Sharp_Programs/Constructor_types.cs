58ca57cb97bf9cb291e5cfb94a9475add9ed97ce
Vaishnavi Adat <vaishnaviadat@gmail.com>
2025-11-18 14:55:56 +0530

    C# Program

---
+++ C_Sharp_Programs/Constructor_types.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Programs
+{
+    internal class Constructor_types
+    {
+        public Constructor_types()//Default Constructor
+        {
+            Console.WriteLine("hello world");
+            Console.ReadLine();
+        }
+        static void Main(string[] args)
+        {
+            
+            Constructor_types c = new Constructor_types();
+        }
+    }
+}
