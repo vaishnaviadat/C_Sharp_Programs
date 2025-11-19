46b2964777c072d08d232e89d2c727849f471115
Vaishnavi Adat <vaishnaviadat@gmail.com>
2025-11-19 16:35:05 +0530

    C# Program

---
+++ C_Sharp_Programs/Single_Level_Inhritances.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Programs
+{
+    internal class Single_Level_Inhritances
+    {   class Person
+        {
+            public int Age;
+            public string Name;
+            public void PersonDetails()
+            {
+                Console.WriteLine($"Name:{Name}, Age:{Age}");
+            }
+        }
+        class Student : Person
+        {
+            public int StudentID;
+            public void StudentDetails()
+            {
+                Console.WriteLine($"StudentID:{StudentID}");
+            }
+        }
+        static void Main(string[] args)
+
+        {
+            Student s = new Student();
+            s.Name = "Vaishna";
+            s.Age = 21;
+            s.StudentID = 201;
+            s.PersonDetails();
+            s.StudentDetails();
+            Console.ReadLine();
+        }
+    }
+}
