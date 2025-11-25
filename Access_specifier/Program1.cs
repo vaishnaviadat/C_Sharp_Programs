3918507251a33a0e0f5e5ee7f22dff7357ece453
Vaishnavi Adat <vaishnaviadat@gmail.com>
2025-11-25 15:57:46 +0530

    C# Program

---
+++ Access_specifier/Program1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+//namespace Access_specifier
+
+    //public access specifier
+
+    //public class class1
+    //{
+    //    public void display()
+    //    {
+    //        Console.WriteLine("This is public access specifier");
+    //    }
+    //    //public void show()       // current class
+    //    //{
+    //    //    class1 obj= new class1();
+    //    //    obj.display();
+    //    //}
+    //}
+    //public class Program1:class1
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        //class1 obj= new class1();   
+    //        //obj.show();
+
+    //        Program1 obj1 = new Program1();
+    //        obj1.display();
+    //        Console.ReadLine();
+    //    }
+    //}
+
+    // private access specifier
+    public class class1
+    {
+        private void display()
+        {
+            Console.WriteLine("This is private class");
+        }
+        public void display1()
+        {
+            class1 obj = new class1();
+            obj.display();
+        }
+    }
+    public class Program1
+    {
+        static void Main(string[] args)
+        {
+            class1 obj= new class1();   
+            obj.display1();
+            Console.ReadLine();
+        }
+    }
+
---
+++ Access_specifier2/Program2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Access_specifier;
+
+namespace Access_specifier2
+{
+    //internal class Program2
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        Console.WriteLine("this is second project");
+    //        class1 obj = new class1();
+    //        obj.display();
+    //        Console.ReadLine();
+    //    }
+    //}
+
+
+    internal class Program2:class1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("this is second project-using derived class");
+            Program2 obj = new Program2();
+            obj.display();
+            Console.ReadLine();
+        }
+    }
+}
