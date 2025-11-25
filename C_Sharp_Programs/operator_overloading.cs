2274002dbdedd0b1767d5a1c02f71fbbf93a6d24
Vaishnavi Adat <vaishnaviadat@gmail.com>
2025-11-25 15:02:44 +0530

    C# Program

--- C_Sharp_Programs/operator_overloading.cs
+++ C_Sharp_Programs/operator_overloading.cs
@@ -6,12 +6,38 @@ using System.Threading.Tasks;
 
 namespace C_Sharp_Programs
 {
+    class  newclass
+    {
+        public string str;
+        public int num;
+        public static newclass operator +(newclass obj1,newclass obj2)
+        {
+            newclass obj3=new newclass();
+            obj3.num = obj1.num + obj2.num;
+            obj3.str = obj1.str + obj2.str;
+            return obj3;
+        }
+
+    }
     internal class operator_overloading
     {
 
-        static void Main(string[] args)
+            static void Main(string[] args)
         {
+            newclass obj1 = new newclass();
+            newclass obj2 = new newclass();
+            newclass obj3 = new newclass();
 
+            obj1.str = "vaishna ";
+            obj1.num = 100;
+
+            obj2.str = "adat";
+            obj2.num = 6;
+            obj3 = obj1 + obj2;
+
+            Console.WriteLine("Name is : " + obj3.str);
+            Console.WriteLine("Id is : " + obj3.num);
+            Console.ReadLine();
         }
     }
 }
