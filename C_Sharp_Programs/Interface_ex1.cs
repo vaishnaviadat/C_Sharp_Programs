d3f99dd4fb27acaee4df75a41128deaf46b38294
Vaishnavi Adat <vaishnaviadat@gmail.com>
2025-11-30 11:11:02 +0530

    C# Program

---
+++ C_Sharp_Programs/Interface_ex1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Programs
+{
+    internal class Interface_ex1
+    {
+        interface IPrinter
+        {
+                       void Print(string document,int pages);//abstract method
+            void Scan(string document,string format); //abstract method
+        }
+        class InkjectPrinter : IPrinter
+        {
+            public void Print(string document, int pages)
+            {
+                Console.WriteLine($"Printing {pages} pages of {document} using Inkjet Printer.");
+            }
+            public void Scan(string document, string format)
+            {
+                Console.WriteLine($"Scanning {document} in {format} format using Inkjet Printer.");
+            }
+        }
+        class LaserPrinter : IPrinter
+        {
+            public void Print(string document, int pages)
+            {
+                Console.WriteLine($"Printing {pages} pages of {document} using Laser Printer.");
+            }
+            public void Scan(string document, string format)
+            {
+                Console.WriteLine($"Scanning {document} in {format} format using Laser Printer.");
+            }
+        }
+        static void Main(string[] args)
+
+        {
+            Console.WriteLine("------Inkjet Printer------");
+            IPrinter p1 = new InkjectPrinter();
+            p1.Print("MyDocument", 5);
+            p1.Scan("MyDocument", "PDF");
+
+            Console.WriteLine("------Laser Printer------");
+            LaserPrinter p2 = new LaserPrinter();
+            p2.Print("MyReport", 10);
+            p2.Scan("MyReport", "JPEG");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ C_Sharp_Programs/Interface_ex2.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Programs
+{
+    internal class Interface_ex2
+    {
+        public interface IPayment
+        {
+            void Pay(string from, string to, double amount);
+
+            void SentReceipt(string to, double amount);
+
+            void LogPayment(string method, string status, DateTime time);
+        }
+        class CreditCardPayment : IPayment
+        {
+            public void Pay(string from, string to, double amount)
+            {
+                Console.WriteLine($"Paid {amount} from {from} to {to} using Credit Card.");
+            }
+            public void SentReceipt(string to, double amount)
+            {
+                Console.WriteLine($"Sent receipt of {amount} to {to} for Credit Card payment.");
+            }
+            public void LogPayment(string method, string status, DateTime time)
+            {
+                Console.WriteLine($"Payment Method: {method}, Status: {status}, Time: {time}");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("------Payment Details------");
+            IPayment payment = new CreditCardPayment();
+            payment.Pay("Alice", "John", 180.75);
+            payment.SentReceipt("John", 180.75);
+            payment.LogPayment("Credit Card", "Successful", DateTime.Now);
+            Console.ReadLine();
+        }
+
+    }
+}
