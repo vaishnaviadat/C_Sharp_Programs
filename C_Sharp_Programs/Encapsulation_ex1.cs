8cd4bea39728b992af7b388ee848842564c5db3f
Vaishnavi Adat <vaishnaviadat@gmail.com>
2025-12-01 14:35:16 +0530

    C# Program

---
+++ C_Sharp_Programs/Encapsulation_ex1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Programs
+{
+    internal class Encapsulation_ex1
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+    }
+}
