16b0162747251c5e440f7882205e327897b2c2b1
Vaishnavi Adat <vaishnaviadat@gmail.com>
2025-12-03 16:28:56 +0530

    C# Program

---
+++ C_Sharp_Programs/Non_Generic_Queue_Collection.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Programs
+{
+    internal class Non_Generic_Queue_Collection     //FIFO
+    {
+        static void Main(string[] args)
+        {
+            Queue q1 = new Queue();
+            q1.Enqueue("Neha");
+            q1.Enqueue("Siya");
+            q1.Enqueue(106);
+            q1.Enqueue(102030);
+            q1.Enqueue(true);
+            Console.WriteLine("--------Enqueue Operation-------------");
+
+            foreach (var item in q1)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("Count is:" + q1.Count);
+            q1.Dequeue();
+            Console.WriteLine("--------Dequeue Operation-------------");
+
+            foreach (var item in q1)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("Count is:" + q1.Count);
+
+            Console.WriteLine("---------------------");
+
+            Console.ReadLine();
+        }
+    }
+}
