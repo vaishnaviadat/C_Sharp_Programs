e5c38305ead3c06908b74eb63595fa254635854a
Vaishnavi Adat <vaishnaviadat@gmail.com>
2025-12-04 15:24:52 +0530

    C# Program

---
+++ C_Sharp_Programs/Non_generic_Sorted_List_Collection.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Programs
+{
+    internal class Non_generic_Sorted_List_Collection
+    {
+        static void display_list(SortedList s1)
+        {
+            foreach(DictionaryEntry item in s1)
+            {
+                Console.WriteLine($"Id :{item.Key}, Name:{item.Value}");
+            }
+        }
+        static void Main(string[] args)
+        {
+            SortedList l1=new SortedList();
+            l1.Add(6, "vaishna");
+            l1.Add(3, "prisha");
+            l1.Add(1, "Saisha");
+            l1.Add(2, "Ram");
+            Console.WriteLine("----------Sorted List--------");
+            display_list(l1);
+            Console.ReadLine();
+        }
+    }
+}
