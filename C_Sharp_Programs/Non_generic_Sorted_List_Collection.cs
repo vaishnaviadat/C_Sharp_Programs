f2dea341f2cac6f49484e6fce892337c6b95442d
Vaishnavi Adat <vaishnaviadat@gmail.com>
2025-12-04 15:34:42 +0530

    C# Program

--- C_Sharp_Programs/Non_generic_Sorted_List_Collection.cs
+++ C_Sharp_Programs/Non_generic_Sorted_List_Collection.cs
@@ -22,8 +22,21 @@ namespace C_Sharp_Programs
             l1.Add(3, "prisha");
             l1.Add(1, "Saisha");
             l1.Add(2, "Ram");
+            l1.Add(5, "Krishna");
+
             Console.WriteLine("----------Sorted List--------");
             display_list(l1);
+
+            Console.WriteLine("Value of Key:" + l1[3]);
+            Console.WriteLine("Value of Index:"+l1.GetByIndex(1));
+            l1[4] = "Neha";
+            display_list(l1);
+
+            Console.WriteLine("----------Remove data--------");
+            l1.Remove(1);
+            display_list(l1);
+
+            Console.WriteLine("\nCount is:" + l1.Count);
             Console.ReadLine();
         }
     }
