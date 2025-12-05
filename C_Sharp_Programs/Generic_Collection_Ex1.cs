56e5dc8e8d25a9bed4f6d655315260d79b795dc7
Vaishnavi Adat <vaishnaviadat@gmail.com>
2025-12-05 15:03:58 +0530

    C# Program

--- C_Sharp_Programs/Generic_Collection_Ex1.cs
+++ C_Sharp_Programs/Generic_Collection_Ex1.cs
@@ -8,6 +8,14 @@ namespace C_Sharp_Programs
 {
     internal class Generic_Collection_Ex1
     {
+        static void printcitylist(List<string> citylist)
+        {
+            Console.WriteLine("----------Current List-----------");
+            foreach (string i in citylist)
+            {
+                Console.WriteLine(i);
+            }
+        }
         static void Main(string[] args)
         {
             List<string> citylist = new List<string>();
@@ -16,6 +24,8 @@ namespace C_Sharp_Programs
             citylist.Add("Mumbai");
             citylist.Add("Pune");
             citylist.Add("New Delhi");
+           
+            printcitylist(citylist);
             Console.ReadLine();
         }
     }
