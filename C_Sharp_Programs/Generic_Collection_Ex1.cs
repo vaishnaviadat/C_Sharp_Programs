c9baf952337646371e981868d896ff01a8c5f89f
Vaishnavi Adat <vaishnaviadat@gmail.com>
2025-12-05 14:58:50 +0530

    C# Program

---
+++ C_Sharp_Programs/Generic_Collection_Ex1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Programs
+{
+    internal class Generic_Collection_Ex1
+    {
+        static void Main(string[] args)
+        {
+            List<string> citylist = new List<string>();
+            citylist.Add("London");
+            citylist.Add("New York");
+            citylist.Add("Mumbai");
+            citylist.Add("Pune");
+            citylist.Add("New Delhi");
+            Console.ReadLine();
+        }
+    }
+}
