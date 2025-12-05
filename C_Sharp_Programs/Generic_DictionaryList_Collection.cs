8c09f0f8b9407c0a6696a9bf5ccb9977b36b77aa
Vaishnavi Adat <vaishnaviadat@gmail.com>
2025-12-05 15:49:25 +0530

    C# Program

--- C_Sharp_Programs/Generic_DictionaryList_Collection.cs
+++ C_Sharp_Programs/Generic_DictionaryList_Collection.cs
@@ -8,6 +8,13 @@ namespace C_Sharp_Programs
 {
     internal class Generic_DictionaryList_Collection
     {
+        static void stulist(Dictionary<int, string> dict)
+        {
+            foreach (var i in dict)
+            {
+                Console.WriteLine("ID:{0} Name:{1}", i.Key, i.Value);
+            }
+        }
         static void Main(string[] args)
         {
             Dictionary<int, string> dict = new Dictionary<int, string>();
@@ -16,10 +23,15 @@ namespace C_Sharp_Programs
             dict.Add(103, "Rani");
             dict.Add(104, "Ram");
             dict.Add(105, "Anjali");
-            foreach (KeyValuePair<int, string> kvp in dict)
-            {
-                Console.WriteLine("Key:{0},Value:{1}", kvp.Key, kvp.Value);
-            }
+           
+            Console.WriteLine("-------Student List--------");
+            stulist(dict);
+
+            Console.WriteLine("-------Contains Key--------");//Check key is present or not
+            Console.WriteLine("Contains Key 103: " + dict.ContainsKey(103));
+
+            Console.WriteLine("-------Contains Value--------");//Check value is present or not
+            Console.WriteLine("Contains Value 'Prisha': " + dict.ContainsValue("Prisha"));
             Console.ReadLine();
         }
     }
