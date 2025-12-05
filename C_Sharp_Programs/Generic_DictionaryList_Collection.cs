061cb2cee3ffd9785e9de50191cfffe36476ffd1
Vaishnavi Adat <vaishnaviadat@gmail.com>
2025-12-05 15:40:52 +0530

    C# Program

--- C_Sharp_Programs/Generic_DictionaryList_Collection.cs
+++ C_Sharp_Programs/Generic_DictionaryList_Collection.cs
@@ -11,9 +11,15 @@ namespace C_Sharp_Programs
         static void Main(string[] args)
         {
             Dictionary<int, string> dict = new Dictionary<int, string>();
-            dict.Add(1, "Apple");
-            dict.Add(2, "Banana");
-
+            dict.Add(101, "Siya");
+            dict.Add(102, "Radha");
+            dict.Add(103, "Rani");
+            dict.Add(104, "Ram");
+            dict.Add(105, "Anjali");
+            foreach (KeyValuePair<int, string> kvp in dict)
+            {
+                Console.WriteLine("Key:{0},Value:{1}", kvp.Key, kvp.Value);
+            }
             Console.ReadLine();
         }
     }
