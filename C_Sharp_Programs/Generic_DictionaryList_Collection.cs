db9d24c262c6252f19869b8a59978ccf7962b923
Vaishnavi Adat <vaishnaviadat@gmail.com>
2025-12-05 15:38:51 +0530

    C# Program

---
+++ C_Sharp_Programs/Generic_DictionaryList_Collection.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Programs
+{
+    internal class Generic_DictionaryList_Collection
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> dict = new Dictionary<int, string>();
+            dict.Add(1, "Apple");
+            dict.Add(2, "Banana");
+
+            Console.ReadLine();
+        }
+    }
+}
