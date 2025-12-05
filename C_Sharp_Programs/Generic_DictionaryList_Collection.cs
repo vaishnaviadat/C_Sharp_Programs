a3b56268c7c94f3ff0d9c9ccb2b25f1b6121fd92
Vaishnavi Adat <vaishnaviadat@gmail.com>
2025-12-05 16:00:35 +0530

    C# Program

--- C_Sharp_Programs/Generic_DictionaryList_Collection.cs
+++ C_Sharp_Programs/Generic_DictionaryList_Collection.cs
@@ -26,12 +26,40 @@ namespace C_Sharp_Programs
            
             Console.WriteLine("-------Student List--------");
             stulist(dict);
+            Console.WriteLine();
 
             Console.WriteLine("-------Contains Key--------");//Check key is present or not
             Console.WriteLine("Contains Key 103: " + dict.ContainsKey(103));
+            Console.WriteLine();
 
             Console.WriteLine("-------Contains Value--------");//Check value is present or not
             Console.WriteLine("Contains Value 'Prisha': " + dict.ContainsValue("Prisha"));
+            Console.WriteLine();
+
+            Console.WriteLine("-------Update Value--------");
+            dict[104] = "Saisha";//Update value at key 104
+            stulist(dict);
+            Console.WriteLine();
+
+            Console.WriteLine("-------Remove Index--------");
+            dict.Remove(103);//Remove key 102
+            stulist(dict);
+            Console.WriteLine();
+
+            Console.WriteLine("--------only keys---------");//print only keys
+            foreach (var key in dict.Keys)
+            {
+                Console.WriteLine("Key:"+key);
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("--------only Values---------");//print only Values
+            foreach (var value in dict.Values)
+            {
+                Console.WriteLine("Value:" + value);
+            }
+            Console.WriteLine();
+
             Console.ReadLine();
         }
     }
