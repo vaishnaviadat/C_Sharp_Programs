c2d8895d629ce6d8180f48d6889a1eae29f791d0
Vaishnavi Adat <vaishnaviadat@gmail.com>
2025-12-05 16:12:39 +0530

    C# Program

---
+++ C_Sharp_Programs/Generic_Stack.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Programs
+{
+    internal class Generic_Stack
+    {
+        static void printnumbers(Stack<int> stack)
+        {
+            foreach (int i in stack)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Stack<int> stack = new Stack<int>();
+            stack.Push(101);
+            stack.Push(102);
+            stack.Push(103);
+            stack.Push(104);
+            stack.Push(105);
+            stack.Push(106);
+            Console.WriteLine("Stack elements:");
+
+            printnumbers(stack);
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
