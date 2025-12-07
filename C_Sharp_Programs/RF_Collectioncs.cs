8b6513779ea1a01de740c6d63dd367da6ba3f321
Vaishnavi Adat <vaishnaviadat@gmail.com>
2025-12-07 14:29:50 +0530

    C# Program

--- C_Sharp_Programs/RF_Collectioncs.cs
+++ C_Sharp_Programs/RF_Collectioncs.cs
@@ -21,8 +21,8 @@ namespace C_Sharp_Programs
             ArrayList a1=new ArrayList();
             a1.Add(10);
             a1.Add("vaishna");
-            a1.Add(15.5);
-            a1.Add('A');
+            a1.Add(15);
+            a1.Add(1);
             a1.Add(106);
             Console.WriteLine("-------Arraylist elements----------");
             printarraylist(a1);
@@ -33,7 +33,43 @@ namespace C_Sharp_Programs
             printarraylist(a1);
             Console.WriteLine();
 
+            Console.WriteLine("-----Arraylist elements after removing element------");
+            a1.Remove("vaishna");
+            printarraylist(a1);
+            Console.WriteLine();
 
+            Console.WriteLine("-----Arraylist elements after removing element at index 2------");
+            a1.RemoveAt(2);
+            printarraylist(a1);
+            Console.WriteLine();
+
+            Console.WriteLine("-----Arraylist elements after sorting------");
+            a1.Sort();
+            printarraylist(a1);
+            Console.WriteLine();
+
+            Console.WriteLine("-----Arraylist elements after reversing------");
+            a1.Reverse();
+            printarraylist(a1);
+            Console.WriteLine();
+
+            Console.WriteLine("-----Arraylist Count------");
+            Console.WriteLine("Count is: " + a1.Count);
+            Console.WriteLine();
+
+            Console.WriteLine("-----Arraylist Contains------");
+            Console.WriteLine("Contains 106: " + a1.Contains(106));
+            Console.WriteLine();
+
+            Console.WriteLine("-----Arraylist ToArray------");
+            object[]array=a1.ToArray();
+            Console.WriteLine("to array is:"+ array);
+            Console.WriteLine();
+
+            Console.WriteLine("-----Arraylist Clear------");
+            a1.Clear();
+            printarraylist(a1); 
+            Console.WriteLine("clear element");
             Console.ReadLine();
 
         }
