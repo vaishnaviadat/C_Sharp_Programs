c575a77d86ed205313985e64fec807794b1901a3
Vaishnavi Adat <vaishnaviadat@gmail.com>
2025-12-08 21:58:22 +0530

    C# Program

--- C_Sharp_Programs/Non_Generic_HashTable_Collection.cs
+++ C_Sharp_Programs/Non_Generic_HashTable_Collection.cs
@@ -23,7 +23,6 @@ namespace C_Sharp_Programs
             products.Add(103, "Tablet");
             products.Add(104, "Monitor");
             products.Add(105, "Keyboard");
-            products.Add(106, "Mouse");
 
             Console.WriteLine("----------Product List-----------");// hashtable display data in any order
 
