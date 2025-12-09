7b93d1a6b2b4891bea745f5b99759ad869b22e99
Vaishnavi Adat <vaishnaviadat@gmail.com>
2025-12-09 15:17:47 +0530

    C# Program

---
+++ C_Sharp_Programs/Generic_SortedList_Collection.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Programs
+{
+    internal class Generic_SortedList_Collection
+    {
+        static void Main(string[] args)
+        {
+            // Creating a SortedList of int keys and string values
+            SortedList<int, string>students = new SortedList<int, string>();
+            Console.ReadLine();
+        }
+    }
+}
