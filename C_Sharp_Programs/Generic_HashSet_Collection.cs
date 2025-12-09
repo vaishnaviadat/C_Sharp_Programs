723080b907950b9f6f6dc20ef519df2fb3985b5d
Vaishnavi Adat <vaishnaviadat@gmail.com>
2025-12-09 14:45:23 +0530

    C# Program

---
+++ C_Sharp_Programs/Generic_HashSet_Collection.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Programs
+{
+    internal class Generic_HashSet_Collection
+    {
+        static void printfruits(HashSet<string> fruits)
+        {
+            foreach (string fruit in fruits)
+            {
+                Console.WriteLine(fruit);
+            }
+        }
+        static void Main(string[] args)
+        {
+            HashSet<string> fruits = new HashSet<string>();
+            fruits.Add("Apple");
+            fruits.Add("Banana");
+            fruits.Add("Mango");
+            fruits.Add("Orange");
+            fruits.Add("Grapes");
+            fruits.Add("Apple"); // Duplicate, will not be added
+            Console.WriteLine("----------Fruit List-----------");
+            printfruits(fruits);
+            Console.ReadLine();
+        }
+    }
+}
