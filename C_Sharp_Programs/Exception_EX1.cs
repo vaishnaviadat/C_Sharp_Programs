688190141778b5c1c597b5dbaa514ed1f04fc071
Vaishnavi Adat <vaishnaviadat@gmail.com>
2025-12-10 14:57:05 +0530

    C# Program

---
+++ C_Sharp_Programs/Exception_EX1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Programs
+{
+    internal class Exception_EX1
+    {
+        static void Main(string[]args)
+        {
+            Console.WriteLine("Enter first number:");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter second number:");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            try
+            {
+                int result = num1 / num2;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            finally
+            {
+                Console.WriteLine("Execution completed.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
