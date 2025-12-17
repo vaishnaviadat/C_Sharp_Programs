05229967b9e5b82d08744efcb061d4e5ae4dcb2e
Vaishnavi Adat <vaishnaviadat@gmail.com>
2025-12-17 14:55:46 +0530

    C# Program

---
+++ C_Sharp_Programs/File1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;    
+namespace C_Sharp_Programs
+{
+    internal class File1
+    {
+        static void Main(string[] args)
+        {
+            string path= @"C:\File\f1.txt";//verbatim string literal
+            string path1 = @"C:\File\f3.txt";//verbatim string literal
+
+            if (File.Exists(path1))
+            {
+                Console.WriteLine("File Exists");
+            }
+            else
+            {
+                Console.WriteLine("File does not Exists");
+            }
+            Console.ReadLine();
+        }
+    }
+}
